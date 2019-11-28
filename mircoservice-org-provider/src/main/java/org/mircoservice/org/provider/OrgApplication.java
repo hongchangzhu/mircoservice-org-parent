083d805dd1dce7e2f932a483950bb81698199205
chenhao <chenhao@authine.com>
2019-11-28 18:04:27 +0800

    111

--- mircoservice-org-provider/src/main/java/org/mircoservice/org/provider/OrgApplication.java
+++ mircoservice-org-provider/src/main/java/org/mircoservice/org/provider/OrgApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"org.mircoservice.org"})
 @EnableDiscoveryClient
 public class OrgApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(OrgApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(OrgApplication.class, args);
+    }
 }
--- mircoservice-org-provider/src/main/java/org/mircoservice/org/provider/rest/OrgController.java
+++ mircoservice-org-provider/src/main/java/org/mircoservice/org/provider/rest/OrgController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class OrgController implements OrgService {
-
+    //@RequestMapping(value = "/org/query", method = RequestMethod.GET)
     public String queryOrg(String id) {
         return "中国深圳-after-" + id;
     }
